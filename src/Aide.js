afacf2e991f14fffb268404e4fc55ef690c82975
Qu3nt174 <quentindray74@hotmail.fr>
2020-06-03 16:28:08 +0200

    fautes corrigées, bouton transparent (faire classe correctement des liens)

--- src/Aide.js
+++ src/Aide.js
@@ -17,7 +17,7 @@ function Search(){
             <h1 id="oui">
                Laissez-nous
                <br></br>
-               Vous aidez
+               vous aider ! 
            </h1>
          
                  
